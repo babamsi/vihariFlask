659353b506178fbe09caeda071bd2d18ba29db72
Bamci <bamci@Bamcis-MacBook-Pro.local>
2024-04-30 21:19:43 +0530

    vihariAPI

---
+++ app.py
@@ -0,0 +1,262 @@
+from flask import Flask, jsonify, request;
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from bson.objectid import ObjectId
+from bson import json_util
+import json
+import os
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://bamsi:Alcuduur40@cluster0.vtlehsn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", server_api=ServerApi('1'))
+db = client["vihari"]
+CORS(app)
+
+@app.route('/createZone', methods=["POST"])
+def zone():
+    incoming_msg = request.get_json();
+    zone = db['Zone']
+    admin = db["Admins"]
+    driver = list(db["Driver"].find({"firstname": "Driver" }))
+    zone_admin = admin.find_one({"firstname": "Bamsi"})
+    zone_dict = {
+        "zone_name": incoming_msg['zoneName'],
+        "added_by": "Admin",
+        "geofence_radius": '',
+        "price_matrix": [],
+        "total_vehicles": [],
+        "total_drivers": driver,
+        "status": ""
+    }
+
+    zone.insert_one(zone_dict)
+    # print(driver)
+    # print(driver.find())
+    
+    return "working...."
+
+
+@app.route('/getZones')
+def getzones():
+    zones = db['Zone']
+    
+    zone = zones.find()
+    zone_list = list(zone)
+    # for items in zone:
+    #     zone_dict = {
+    #         "zone_name": items["zone_name"],
+    #         "geofence_radius": items["geofence_radius"],
+    #         "price_matrix": items["price_matrix"],
+    #         "total_vehicles": items["total_vehicles"],
+    #         "total_drivers": items["total_drivers"]
+    #     }
+    # zone_list.append(zone_dict)
+    
+    
+    return json.loads(json_util.dumps(zone_list))
+
+
+@app.route('/getVendors')
+def getvendors():
+    vendors = db['Vendors']
+    
+    vendor = vendors.find()
+    vendor_list = list(vendor)
+    # for items in zone:
+    #     zone_dict = {
+    #         "zone_name": items["zone_name"],
+    #         "geofence_radius": items["geofence_radius"],
+    #         "price_matrix": items["price_matrix"],
+    #         "total_vehicles": items["total_vehicles"],
+    #         "total_drivers": items["total_drivers"]
+    #     }
+    # zone_list.append(zone_dict)
+    
+    
+    return json.loads(json_util.dumps(vendor_list))
+
+@app.route('/getDrivers')
+def getDrivers():
+    drivers = db['Driver']
+    
+    driver = drivers.find()
+    driver_list = list(driver)
+    # for items in zone:
+    #     zone_dict = {
+    #         "zone_name": items["zone_name"],
+    #         "geofence_radius": items["geofence_radius"],
+    #         "price_matrix": items["price_matrix"],
+    #         "total_vehicles": items["total_vehicles"],
+    #         "total_drivers": items["total_drivers"]
+    #     }
+    # zone_list.append(zone_dict)
+    
+    
+    return json.loads(json_util.dumps(driver_list))
+
+@app.route('/createDriver', methods=["POST"])
+def createDriver():
+    incoming_msg = request.get_json()["Body"];
+    drivers = db["Driver"]
+    zone = db["Zone"]
+    zone = db["Zone"].find_one({"zone_name": incoming_msg["zone"]})
+    driver_dict = {
+        "firstname": incoming_msg["firstName"],
+        "lastname": incoming_msg["lastName"],
+        "mobile": incoming_msg["mobile"],
+        "alt_mobile": incoming_msg["altNumber"],
+        "email": incoming_msg["email"],
+        "zone": zone,
+        "license_number":incoming_msg["licenseNumber"],
+        "driving_license":incoming_msg["drivingPhoto"],
+        "id_proof_front_url": incoming_msg["imgUrl"],
+        "address_proof_url": incoming_msg["addressProof"],
+        "pan_card": incoming_msg["pan"],
+        "calender_availability": ""
+    }
+    drivers.insert_one(driver_dict)
+
+    return "working..."
+
+@app.route('/createAdmin')
+def createAdmin():
+    # incoming_msg = request.get_json();
+    
+    admin = db['Admins']
+    zones = db["Zone"]
+    zoneAdmin = zones.find_one({"zone_admin.name": "Bamsi"})
+    admin_dict = {
+        "firstname": "Bamsi",
+        "lastName": "c",
+        "contact": "917710285988",
+        "zone": zoneAdmin['zone_name'] if zoneAdmin else "",
+        "email": "hh989940@gmail.com",
+        "license_number": "",
+        "role": ''
+    }
+    admin.insert_one(admin_dict)
+    # print(zoneAdmin)
+    return "working..."
+
+
+@app.route('/createCustomer')
+def createCustomer():
+    customer = db['Customer']
+
+    customer_dict = {
+        "firstname": "customer",
+        "lastname": "customer",
+        "mobile": "91**********",
+        "email": "*****@***.com",
+        "location": {
+            "lat": 1234532.343,
+            "long": 542532.234
+        },
+        "search_history": [],
+        "booking_history": [],
+        "total_payments": "",
+        "pending_payments": "",
+        "feedback": [],
+        "status": "",
+        "profile_url": ""
+    }
+
+    customer.insert_one(customer_dict)
+    return "working...."
+
+@app.route('/createVendor', methods=["POST"])
+def createVendor():
+    incoming_msg = request.get_json()["Body"];
+    vendors = db['Vendors']
+    zone = db["Zone"].find_one({"zone_name": incoming_msg["zone"]})
+    vendors_dict = {
+        "zone_id": zone['_id'],
+        "firstname": incoming_msg["firstName"],
+        "lastname": incoming_msg["lastName"],
+        "mobile": incoming_msg["mobile"],
+        "alt_mobile": incoming_msg["altNumber"],
+        "email": incoming_msg["email"],
+        "zone": zone,
+        "license_number": incoming_msg["licenseNumber"],
+        "driving_license_front_url": incoming_msg["drivingPhoto"],
+        "driving_license_back_url": "",
+        "address_proof_url": incoming_msg["imgUrl"],
+        "id_proof_front_url" :"",
+        "id_proof_back_url": "",
+        "profile_url": incoming_msg["profilePic"],
+        "pan_card": ""
+        
+    }
+
+    vendors.insert_one(vendors_dict)
+    # print(incoming_msg)
+    return "working....."
+
+@app.route('/createZoneAdmin', methods=["POST"])
+def createZoneAdmin():
+    incoming_msg = request.get_json()["Body"];
+    zone_admins = db['ZoneAdmins']
+    zone = db["Zone"].find_one({"zone_name": incoming_msg["zone"]})
+    vendors_dict = {
+        "zone_id": zone['_id'],
+        "firstname": incoming_msg["firstName"],
+        "lastname": incoming_msg["lastName"],
+        "mobile": incoming_msg["mobile"],
+        "alt_mobile": incoming_msg["altNumber"],
+        "email": incoming_msg["email"],
+        "zone": zone,
+        "license_number": incoming_msg["licenseNumber"],
+        "driving_license_front_url": incoming_msg["drivingPhoto"],
+        "driving_license_back_url": "",
+        "address_proof_url": incoming_msg["imgUrl"],
+        "id_proof_front_url" :"",
+        "id_proof_back_url": "",
+        "profile_url": incoming_msg["profilePic"],
+        "pan_card": ""
+        
+    }
+
+    zone_admins.insert_one(vendors_dict)
+    # print(incoming_msg)
+    return "working....."
+
+@app.route('/createVehicle', methods=["POST"])
+def createVehicle():
+    incoming_msg = request.get_json()["Body"];
+    zone_admins = db['Vehicles']
+    zone = db["Zone"].find_one({"zone_name": incoming_msg["zone"]})
+    driver = db['Driver'].find_one({"firstname": incoming_msg["driverId"]})
+    vehicle_dict = {
+        "zone_id": zone['_id'],
+        "vehicle_name": incoming_msg["vehicleName"],
+        "vehicle_model": incoming_msg["vehicleModel"],
+        "vehicle_type": incoming_msg["vehicleType"],
+        "brand": incoming_msg["brand"],
+        "capacity": incoming_msg["capacity"],
+        "zone": zone,
+        "mileage": incoming_msg["mileage"],
+        "vehicle_owner": incoming_msg["ownerType"],
+        "cost_per_hour": incoming_msg["costPerHour"],
+        "driver_id": driver['_id'],
+        "added_by" :incoming_msg["addedBy"],
+        "vehicle_calender_availability": "",
+        "status": "",
+        "rc_certificate": incoming_msg['rcCertificateUrl'],
+        "premit_certificate": incoming_msg["permitCertificateUrl"],
+        "fitness_certificate": incoming_msg["fitnessCertificateUrl"],
+        "insurance_certificate": incoming_msg["insuranceCertificateUrl"],
+        "pollution_certificate": incoming_msg["pollutionCertificateUrl"]
+        
+    }
+
+    zone_admins.insert_one(vehicle_dict)
+    # print(driver['_id'])
+    return "working....."
+
+if __name__ == '__main__':
+    app.run()
